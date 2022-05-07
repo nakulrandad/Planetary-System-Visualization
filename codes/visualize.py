347ec3994e5795eefbc73db054f5608c8b5269f9
nakulrandad <nakulrandad@gmail.com>
2022-05-08 05:21:47 +0530

    table added

--- codes/visualize.py
+++ codes/visualize.py
@@ -50,76 +50,9 @@ def curve(n_mer, n_long, n_s):
     t = mu
     return x, y, z, t
 
-
-class PlanetarySystemModel(HasTraits):
-    n_meridional = Range(1, 30, 1)
-    n_longitudinal = Range(0, 30, 12)
-    n_size = Range(1, 5, 3)
-
-    scene = Instance(MlabSceneModel, ())
-    plot = Instance(PipelineBase)
-
-    toggle_bg = Button('Dark Mode')
-
-    employees = List(Employee)
-
-    # traits_view = View(
-    #     Group(
-    #         Item('employees', show_label=False, editor=table_editor),
-    #         show_border=True,
-    #     ),
-    #     title='Department Personnel',
-    #     width=0.4,
-    #     height=0.4,
-    #     resizable=True,
-    #     buttons=['OK'],
-    #     kind='live',
-    # )
-
-    def __init__(self):
-        super().__init__()
-        x, y, z, t = curve(self.n_meridional, self.n_longitudinal, self.n_size)
-        self.plot = self.scene.mlab.plot3d(
-            x, y, z, t, tube_radius=0.08, colormap='Spectral')
-        self.is_dark = False
-        self.scene.scene.background = (1, 1, 1)
-
-    @observe('n_meridional,n_longitudinal,n_size,scene.activated')
-    def update_plot(self, event=None):
-        x, y, z, t = curve(self.n_meridional, self.n_longitudinal, self.n_size)
-        self.plot.mlab_source.trait_set(
-            x=x, y=y, z=z, scalars=t)
-
-    def _toggle_bg_fired(self):
-        print("I am toggled!")
-        bgcolor = (0, 0, 0) if self.is_dark else (1, 1, 1)
-        self.is_dark = not self.is_dark
-        self.scene.scene.background = bgcolor
-
-    # The layout of the dialog created
-    view = View(
-        Group(
-            Item(
-                'scene', editor=SceneEditor(
-                    scene_class=MayaviScene),
-                height=250, width=300, show_label=False),
-            Group(
-                '_', 'n_meridional', 'n_longitudinal', 'n_size', 'toggle_bg',
-                show_border=True, label='Curve Parameters'),
-            Group(
-                Item('employees', show_label=False, editor=table_editor),
-                show_border=True),
-            orientation='vertical'),
-        resizable=True, buttons=[UndoButton, RevertButton],
-        kind='live',
-        title='Project: Planetary Visualization')
-
-
-planet_system_model = PlanetarySystemModel(employees=employees)
-planet_system_model.configure_traits()
-
-
 # A helper class for the 'Department' class below:
+
+
 class Employee(HasTraits):
     first_name = Str()
     last_name = Str()
@@ -161,27 +94,8 @@ table_editor = TableEditor(
 )
 
 
-# The class to be edited with the TableEditor:
-# class Department(HasStrictTraits):
-
-#     employees = List(Employee)
-
-#     traits_view = View(
-#         Group(
-#             Item('employees', show_label=False, editor=table_editor),
-#             show_border=True,
-#         ),
-#         title='Department Personnel',
-#         width=0.4,
-#         height=0.4,
-#         resizable=True,
-#         buttons=['OK'],
-#         kind='live',
-#     )
-
-
 # Create some employees:
-employees = [
+employees_list = [
     Employee(first_name='Jason', last_name='Smith', age=32),
     Employee(first_name='Mike', last_name='Tollan', age=34),
     Employee(
@@ -190,6 +104,70 @@ employees = [
     Employee(first_name='Lyn', last_name='Spitz', age=40),
     Employee(first_name='Greg', last_name='Andrews', age=45),
 ]
+
+
+class PlanetarySystemModel(HasTraits):
+    n_meridional = Range(1, 30, 1)
+    n_longitudinal = Range(0, 30, 12)
+    n_size = Range(1, 5, 3)
+
+    scene = Instance(MlabSceneModel, ())
+    plot = Instance(PipelineBase)
+
+    toggle_bg = Button('Dark Mode')
+    reset = Button()
+
+    employees = List(Employee)
+
+    def __init__(self, employees):
+        super().__init__()
+        x, y, z, t = curve(self.n_meridional, self.n_longitudinal, self.n_size)
+        self.plot = self.scene.mlab.plot3d(
+            x, y, z, t, tube_radius=0.08, colormap='Spectral')
+        self.is_dark = False
+        self.scene.scene.background = (1, 1, 1)
+        self.employees = employees
+
+    @observe('n_meridional,n_longitudinal,n_size,scene.activated')
+    def update_plot(self, event=None):
+        x, y, z, t = curve(self.n_meridional, self.n_longitudinal, self.n_size)
+        self.plot.mlab_source.trait_set(
+            x=x, y=y, z=z, scalars=t)
+
+    def _toggle_bg_fired(self):
+        bgcolor = (0, 0, 0) if self.is_dark else (1, 1, 1)
+        self.scene.scene.background = bgcolor
+        self.is_dark = not self.is_dark
+
+    def _reset_fired(self):
+        self.employees = employees_list
+        print("reset fired")
+
+    view = View(
+        Group(
+            Group(
+                Item(
+                    'scene',
+                    editor=SceneEditor(scene_class=MayaviScene),
+                    height=250, width=300, show_label=False),
+                Group(
+                    '_', 'n_meridional', 'n_longitudinal', 'n_size',
+                    'toggle_bg', show_border=True,
+                    label='Plotting Features'),
+                orientation='vertical'),
+            Group(
+                Item(
+                    'employees', show_label=False,
+                    editor=table_editor), 'reset',
+                show_border=True, label='Planet Data',),
+            orientation='horizontal'),
+        resizable=True,
+        kind='live', title='Project: Planetary Visualization')
+
+
+planet_system_model = PlanetarySystemModel(employees=employees_list)
+planet_system_model.configure_traits()
+
 
 # Create the demo:
 # demo = Department(employees=employees)
